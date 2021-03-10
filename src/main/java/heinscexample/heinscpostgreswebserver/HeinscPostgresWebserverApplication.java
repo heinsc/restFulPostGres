fed4f3402e0473dcc6cb3a22238ccb247b481763
Carsten Heins <heinsc@myVM.eiqgsjdgiixu5n0lrlnxxbfmea.bx.internal.cloudapp.net>
2021-03-10 13:09:37 +0000

    initial commit

---
+++ src/main/java/heinscexample/heinscpostgreswebserver/HeinscPostgresWebserverApplication.java
@@ -0,0 +1,13 @@
+package heinscexample.heinscpostgreswebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeinscPostgresWebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeinscPostgresWebserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heinscexample/heinscpostgreswebserver/HeinscPostgresWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package heinscexample.heinscpostgreswebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeinscPostgresWebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
